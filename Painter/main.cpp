d4f80e000a45acf6658831209308f0129b4d9114
kn71026 <nicolechou9712@gmail.com>
2020-08-23 00:57:12 +0800

    :sparkles: 新增畫布+簡單畫筆功能

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -13,6 +13,17 @@ int main() {
 
 	sf::CircleShape cursor(10);
 	cursor.setFillColor(sf::Color::Black);
+	cursor.setOrigin(10, 10);
+
+	sf::RenderTexture canvas;
+	canvas.create(600, 560);
+	canvas.clear(sf::Color::White);
+
+
+	sf::Vector2f canvasOffset(20, 60);
+	sf::Sprite canvasSprite(canvas.getTexture());
+	canvasSprite.setPosition(canvasOffset);
+
 	while ( window.isOpen() ) {
 		sf::Event evt;
 		if (window.pollEvent(evt)) {
@@ -22,9 +33,15 @@ int main() {
 		}
 
 		sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-		cursor.setPosition(sf::Vector2f(mousePos));
+		cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
+
+		canvas.draw(cursor);
+		canvas.display();
+
 		window.clear(sf::Color::White);
 		window.draw(backgroundSprite);
+		window.draw(canvasSprite);
+		cursor.setPosition(sf::Vector2f(mousePos));
 		window.draw(cursor);
 		window.display();
 
