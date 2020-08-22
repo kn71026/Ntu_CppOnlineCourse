ebe4fd31fe3d97ebc2a859184bd9e55633216de5
kn71026 <nicolechou9712@gmail.com>
2020-08-23 01:16:51 +0800

    :sparkles: 完成基本繪製功能
    有七種顏色可以選

--- Painter/main.cpp
+++ Painter/main.cpp
@@ -15,6 +15,36 @@ int main() {
 	cursor.setFillColor(sf::Color::Black);
 	cursor.setOrigin(10, 10);
 
+
+	std::vector<sf::Color> Colors = {
+		sf::Color::White,
+		sf::Color::Blue,
+		sf::Color::Red,
+		sf::Color::Green,
+		sf::Color::Yellow,
+		sf::Color::Cyan,
+		sf::Color::Magenta
+
+	};
+
+	std::vector<sf::RectangleShape> swatches;
+
+	int swatchOffset = 20;
+
+
+	for (auto color : Colors)
+	{
+		sf::RectangleShape swatch(sf::Vector2f(20, 20));
+		swatch.setFillColor(color);
+		swatch.setOutlineColor(sf::Color(230, 230, 230));
+		swatch.setOutlineThickness(2);
+		swatch.setPosition(swatchOffset, 10);
+		swatches.push_back(swatch);
+
+		swatchOffset += 25;
+
+	}
+
 	sf::RenderTexture canvas;
 	canvas.create(600, 560);
 	canvas.clear(sf::Color::White);
@@ -24,22 +54,43 @@ int main() {
 	sf::Sprite canvasSprite(canvas.getTexture());
 	canvasSprite.setPosition(canvasOffset);
 
-	while ( window.isOpen() ) {
+	while (window.isOpen()) {
 		sf::Event evt;
 		if (window.pollEvent(evt)) {
 			if (evt.type == sf::Event::Closed) {
 				window.close();
 			}
+			if (evt.type == sf::Event::MouseButtonPressed) {
+				if (evt.mouseButton.button == sf::Mouse::Left) {
+					evt.mouseButton.x;
+					evt.mouseButton.y;
+
+					for (const auto& swatch : swatches) {
+						if(swatch.getGlobalBounds().contains(
+							evt.mouseButton.x,
+							evt.mouseButton.y)){
+							cursor.setFillColor(swatch.getFillColor());
+}
+
+					}
+				}
+			}
 		}
 
 		sf::Vector2i mousePos = sf::Mouse::getPosition(window);
-		cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
 
+		cursor.setPosition(sf::Vector2f(mousePos) - canvasOffset);
 		canvas.draw(cursor);
 		canvas.display();
+		}
 
 		window.clear(sf::Color::White);
 		window.draw(backgroundSprite);
+
+		for (const auto& swatch : swatches) {
+			window.draw(swatch);
+		}
 		window.draw(canvasSprite);
 		cursor.setPosition(sf::Vector2f(mousePos));
 		window.draw(cursor);
